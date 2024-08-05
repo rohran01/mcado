ef9148cb47bcdd44ecb81c64ee3f64591dc8eb4d
Anthony Rohr <anthonytrohr@gmail.com>
2024-08-05 13:44:17 -0500

    beginnings or rapture plus app

--- src/app/rapture/rapture.component.ts
+++ src/app/rapture/rapture.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-rapture',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './rapture.component.html',
   styleUrl: './rapture.component.scss'
 })
